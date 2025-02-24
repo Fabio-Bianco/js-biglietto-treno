4fd2f2451f9829f467073e5b07335e26f59fd5f2
Fabio-Bianco <biancobfabio@gmail.com>
2025-02-24 18:14:19 +0100

    scaffolding ex

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('prova')
+
+
+const milesToGo = prompt ('Quanti chilometri dista la tua destinazione?');
+console.log = 'Quanti chilometri dista la tua destinazione?';
+const userAge = prompt (`Qual'è la tua età?`);
+
+
