67f41e2ed9439df1b039a5dd183388169d1f174d
Fabio-Bianco <biancobfabio@gmail.com>
2025-02-24 19:14:08 +0100

    feat promt: 'miles to go 'and 'user age';

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
 
-const milesToGo = prompt ('Quanti chilometri dista la tua destinazione?');
-console.log = 'Quanti chilometri dista la tua destinazione?';
-const userAge = prompt (`Qual'è la tua età?`);
+
+//Chiedo all'utente i chilometri da percorrere e li salvo in una variabile;
+const milesToGo = prompt ('Inserisci i chilometri di distanza oer raggiungere la tua destinazione');
+console.log(milesToGo);
+
+//Chiedo all'utente l'età e la salvo in una variabile;
+const userAge = prompt ('inserisci la tua età');
+console.log(userAge);
+
+
+
+
+
+
 
 
